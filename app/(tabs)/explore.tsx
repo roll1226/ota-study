02b33749e66865afefca0d6c156d9e30833ff86a
roll1226 <tomo_yasu_4724@icloud.com>
2026-02-03 23:08:45 +0900

    HomeScreen: Reorder import statements for consistency and improve formatting

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,6 +1,5 @@
 import { Image } from "expo-image";
 import { Platform, StyleSheet } from "react-native";
-
 import { ExternalLink } from "@/components/external-link";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
